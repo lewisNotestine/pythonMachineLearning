ceb503d66020efb319ee2440183d78f451531aa3
Lewis S Notestine <lewis.notestine@gmail.com>
2014-03-17 00:01:25 -0700

    updated file loading method, now is different from given in text

--- testKnn.py
+++ testKnn.py
@@ -7,3 +7,38 @@ def createDataSet():
     return group, labels
 
 
+def classify0(inX, dataSet, labels, k):
+    """
+    classifies a dataset and labels according to the KNN algorithm. 
+    """
+    dataSetSize = dataSet.shape[0]
+    diffMatrix = tile(inX, (dataSetSize, 1)) - dataSet
+    print diffMatrix
+    sqDiffMatrix = diffMatrix**2
+    print sqDiffMatrix
+    sqDistances = sqDiffMatrix.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteILabel = labels[sortedDistIndices[i]]
+        classCount[voteILabel] = classCount.get(voteILabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(listFromLine[-1])
+        index += 1
+    return returnMat,classLabelVector
+
